256d8236685d0ad13de868126304e2db094e6e09
Svilen Popov <svil4ok@gmail.com>
2025-04-26 19:02:58 +0300

    feat: display the location number and adjust the link

--- src/components/SiteList.tsx
+++ src/components/SiteList.tsx
@@ -21,31 +21,36 @@ export const SiteList = ({ siteList }: SiteListProps) => {
     >
       {siteList.map((site) => (
         <li key={site.name} className="relative">
-          <div className="relative">
-            <div className="relative h-56 w-full overflow-hidden rounded-lg">
-              <img src={site.image} className="size-full object-cover" />
-            </div>
+          <a href={site.link} target="_blank">
+            <div className="relative">
+              <div className="relative h-56 w-full overflow-hidden rounded-lg">
+                <img src={site.image} className="size-full object-cover" />
+              </div>
 
-            <div className="relative mt-4">
-              <h3 className="text-sm font-medium text-gray-900">
-                <a href={site.link} target="_blank">
+              <div className="relative mt-4">
+                <h3 className="text-sm font-medium text-gray-900">
                   {site.name}
-                </a>
-              </h3>
-            </div>
+                </h3>
+              </div>
 
-            <div className="absolute inset-x-0 top-0 flex h-56 items-end justify-end overflow-hidden rounded-lg p-4">
-              <div
-                aria-hidden="true"
-                className="absolute inset-x-0 bottom-0 h-28 bg-linear-to-t from-black opacity-50"
-              />
-              <p className="relative text-lg font-semibold text-white">
-                {site.visited && (
-                  <CheckBadgeIcon title="Посетен обект" className="w-5 h-5" />
-                )}
-              </p>
+              <div className="absolute inset-x-0 top-0 flex h-56 items-end justify-between overflow-hidden rounded-lg p-4">
+                <span className="inline-flex items-center rounded-full bg-indigo-100 px-2 py-1 text-xs font-medium text-indigo-700">
+                  номер {site.number}
+                </span>
+
+                <div
+                  aria-hidden="true"
+                  className="absolute inset-x-0 bottom-0 h-28 bg-linear-to-t from-black opacity-20"
+                />
+
+                <p className="relative text-lg font-semibold text-white">
+                  {site.visited && (
+                    <CheckBadgeIcon title="Посетен обект" className="w-5 h-5" />
+                  )}
+                </p>
+              </div>
             </div>
-          </div>
+          </a>
         </li>
       ))}
     </ul>

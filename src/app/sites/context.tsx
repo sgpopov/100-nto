bfe8d804818670422209b98cccfbe60442716977
Svilen Popov <svil4ok@gmail.com>
2026-03-29 12:49:20 +0300

    fix: do a client side redirect

--- src/app/sites/context.tsx
+++ src/app/sites/context.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import { createContext, useContext } from "react";
-import type { useSiteFilters } from "@/hooks/useSiteFilters";
+import { useSiteFilters } from "@/hooks/useSiteFilters";
 
 type SitesContextValue = ReturnType<typeof useSiteFilters>;
 
